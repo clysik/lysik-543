667121287f2a65c66e388d030684e907b129f715
clysik <celica.lysik@gmail.com>
2026-02-04 15:40:04 -0500

    components

---
+++ components/title.tsx
@@ -0,0 +1,12 @@
+import Body from "@/components/p_body"
+
+export default function Title() {
+  return (
+    <div className="">
+        <h1>Lorem ipsum.</h1>
+        <Body/>
+        <br/>
+        
+    </div>
+  );
+}
