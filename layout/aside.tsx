5422d731fc857c1ecb9ff33e76e6f0add78226c0
clysik <celica.lysik@gmail.com>
2026-02-10 17:54:28 -0500

    Added Layout Components and Adjusted Layout.tsx

---
+++ layout/aside.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react'
+
+const Aside:React.FC = () => {
+    return(
+        <aside style={{padding: '1rem',backgroundColor:'#4ca8ff'}}>
+            <h1>Aside</h1>
+        </aside>
+    );
+};
+
+export default Aside;
---
+++ layout/header.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react'
+
+const Header:React.FC = () => {
+    return(
+        <header style={{padding: '1rem',backgroundColor:'#27ff27'}}>
+            <h1>My Application Header</h1>
+        </header>
+    );
+};
+
+export default Header;
