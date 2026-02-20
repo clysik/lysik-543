6aab1fa7bdb48c2abec7d8af3de5a276abac9e7b
clysik <celica.lysik@gmail.com>
2026-02-19 21:46:33 -0500

    added icon placeholder to aside, added navigation placeholder in header

--- layout/aside.tsx
+++ layout/aside.tsx
@@ -1,9 +1,12 @@
 'use client';
 import React from 'react'
+import Image from 'next/image'
+import Heart from '../assets/whiteheart.png'
 
 const Aside:React.FC = () => {
     return(
         <aside style={{padding: '1rem',backgroundColor:'#ff4cb1'}}>
+            <Image src={Heart} alt="Icon"/>
             <h1>Aside</h1>
             <li>Option 1</li>
             <li>Option 2</li>
--- layout/header.tsx
+++ layout/header.tsx
@@ -5,9 +5,17 @@ import Heart from '../assets/whiteheart.png';
 
 const Header:React.FC = () => {
     return(
-        <header className="flex" style={{padding: '1rem',backgroundColor:'#27ff27'}}>
-            <Image src={Heart} alt="icon"/>
-            <h1>My Application Header</h1>
+        <header className="" style={{padding: '1rem',backgroundColor:'#fcacd0'}}>
+            <div className="flex">
+                <Image src={Heart} alt="icon"/>
+                <h1>My Application Header</h1>
+            </div>
+            <div className="flex p-8" style={{backgroundColor:'#ff3891'}}>
+                <h2>Navigation Option 1</h2>
+                <h2>Navigation Option 2</h2>
+                <h2>Navigation Option 3</h2>
+            </div>
+            
         </header>
     );
 };
